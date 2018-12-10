8bbe53d385a87f2c20f0351c3492a9cf72e3defd
vladimir <vladimirsostaric3390@gmail.com>
2018-12-10 12:32:14 +0100

    added swagger to stringmaster + post filter to zuul gw

---
+++ gateway-zuul/src/main/java/org/locus/learn/gatewayzuul/filters/TestFilter.java
@@ -0,0 +1,40 @@
+package org.locus.learn.gatewayzuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class TestFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("From TestFilter %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+}
